089a35aa4fd9091a47f3d0ab4637542f25c4cd8b
rafael.sgoncalves <rafael.sgoncalves@SM905-CMCIAL216.mateus.dc>
2022-07-19 13:15:12 -0300

    Aula 8.11 feita e testada ok

--- testgit/src/testgit/TesteGit.java
+++ testgit/src/testgit/TesteGit.java
@@ -1,5 +1,21 @@
 package testgit;
 
+import javax.swing.JOptionPane;
+
 public class TesteGit {
+	public static void main(String[] args) {
+		String carros = JOptionPane.showInputDialog("Informe a quantidade de carros");
+		String pessoas = JOptionPane.showInputDialog("Informe a quantidade de Pessoas");
+		
+		double carronumero = Double.parseDouble(carros);
+		double pessoanumero = Double.parseDouble(pessoas); 		
+		
+		int divisao = (int) (carronumero / pessoanumero);
+		
+		double resto = carronumero % pessoanumero;
+		
+		JOptionPane.showMessageDialog(null, " Divisão para pessoas deu " + divisao + " carros e sobrou " + resto + " carros " );
+		
+	}
 
 }
