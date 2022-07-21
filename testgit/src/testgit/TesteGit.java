43f8bf7247b6ed3a5d639df34fbdb2229454252a
rafael.sgoncalves <rafael.sgoncalves@SM905-CMCIAL216.mateus.dc>
2022-07-21 13:38:03 -0300

    Aula 8.13 testado ok

--- testgit/src/testgit/TesteGit.java
+++ testgit/src/testgit/TesteGit.java
@@ -4,36 +4,34 @@ import javax.swing.JOptionPane;
 
 public class TesteGit {
 	public static void main(String[] args) {
-		String carros = JOptionPane.showInputDialog("Informe a quantidade de carros");
-		String pessoas = JOptionPane.showInputDialog("Informe a quantidade de Pessoas");
 		
-		double carronumero = Double.parseDouble(carros);
-		double pessoanumero = Double.parseDouble(pessoas); 		
 		
-		int divisao = (int) (carronumero / pessoanumero);
 		
-		double resto = carronumero % pessoanumero;
 		
-		int resposta = JOptionPane.showConfirmDialog(null, "Deseja ver o resultado? " );
+		String nota1 = JOptionPane.showInputDialog("Informe a nota 1:");
+		String nota2 = JOptionPane.showInputDialog("Informe a nota 2:");
+		String nota3 = JOptionPane.showInputDialog("Informe a nota 3:");
+		String nota4 = JOptionPane.showInputDialog("Informe a nota 4:");
 		
-		if (resposta == 0) {
-			JOptionPane.showConfirmDialog(null, "Divisão para pessoas deu " + divisao);
+		double nNota1 = Double.parseDouble(nota1);
+		double nNota2 = Double.parseDouble(nota2);
+		double nNota3 = Double.parseDouble(nota3);
+		double nNota4 = Double.parseDouble(nota4);
+		
+		double media = (nNota1 + nNota2 + nNota3 + nNota4) / 4;
+		
+		if (media >= 5) {
+			
+			if (media >= 7) {
+				JOptionPane.showMessageDialog(null, "A media é: " + media + " Aprovado!!! ");
+			}else {
+				JOptionPane.showMessageDialog(null, " A media é: " + media + " Você está de recuperação! ");
+			}
 		}else {
-			System.out.println("Não quis ver o resultado!");
+			JOptionPane.showMessageDialog(null, " A media é: " + media + " Você Reprovou, que pena.. ");
 		}
-		
-		resposta = JOptionPane.showConfirmDialog(null, "Deseja ver o resto da divisão? ");
-		if (resposta ==0) {
-			JOptionPane.showMessageDialog(null, "O resto da divisão eh: " + resto);
-		}else {
-			System.out.println("Não quis ver o resultado!! ");
-		}
+					
 				
-		/*Deseja ver o resultado?*/
-		System.out.println(resposta);
-		
-		JOptionPane.showMessageDialog(null, " Divisão para pessoas deu " + divisao + " carros e sobrou " + resto + " carros " );
-		
 	}
 
 }
